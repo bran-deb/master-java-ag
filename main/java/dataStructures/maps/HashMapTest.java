d7874d3876d5beca03e498f7691072b74d8b6dd8
Brand-db <jairo.padilla.deb19@gmail.com>
2022-10-13 21:47:51 -0400

    add: methods HashMap

--- main/java/dataStructures/maps/HashMapTest.java
+++ main/java/dataStructures/maps/HashMapTest.java
@@ -1,7 +1,9 @@
 package dataStructures.maps;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapTest {
     public static void main(String[] args) {
@@ -16,5 +18,56 @@ public class HashMapTest {
         //obtiene los datos a través de sus key
         System.out.println("nombre: " + persona.get("nombre"));
         System.out.println("apellido: " + persona.get("apellido"));//si la llave no existe es null
+
+        System.out.println("\n=====método remove=====");
+        System.out.println(persona);
+//        String datoEliminado = persona.remove("Apellido");
+        boolean datoEliminado = persona.remove("Apellido", "Doe"); //por la key o (key & value)
+        System.out.println("Se elimino correctamente: " + datoEliminado);
+        System.out.println(persona);
+
+        System.out.println("\n=====método containsKey=====");
+        System.out.println(persona);
+        boolean datoEliminado2 = persona.containsKey("Apellido");
+        System.out.println("Se elimino correctamente = " + datoEliminado2);
+        System.out.println(persona);
+
+        System.out.println("\n=====método contains=====");
+        datoEliminado2 = persona.containsValue("john.doe@email.com");
+        System.out.println(persona);
+        System.out.println("contiene el valor de john.doe@email.com");
+        System.out.println("contiene el valor = " + datoEliminado2);
+
+        System.out.println("\n=====iterar values=====");
+        Collection<String> valores = persona.values();
+        valores.forEach(System.out::println);
+
+        System.out.println("\n=====iterar keys=====");
+        Set<String> llaves = persona.keySet();
+        llaves.forEach(System.out::println);
+
+        System.out.println("\n=====iterar keys & values=====");
+        for (Map.Entry<String, String> par : persona.entrySet()) {
+            System.out.println(par.getKey() + "=>" + par.getValue());
+        }
+
+        System.out.println("\n=====iterar keys & values=====");
+        for (String llave : persona.keySet()) {
+            String valor = persona.get(llave);
+            System.out.println(llave + "=>" + valor);
+        }
+
+        System.out.println("\n=====iterar keys & values=====");
+        persona.forEach((llave, valor) -> {
+            System.out.println(llave + "=>" + valor);
+        });
+
+        System.out.println("\n=====otros métodos=====");
+        System.out.println("total: " +persona.size());
+        System.out.println("contiene elementos"+!persona.isEmpty());
+        System.out.println("remplazar elemento");
+        System.out.println(persona);
+        persona.replace("nombre", "Andres");
+        System.out.println(persona);
     }
 }
